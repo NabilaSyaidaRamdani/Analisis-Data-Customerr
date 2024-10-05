25a5373dbe7825cfc144204a503d44cf84c9d964
NabilaSyaidaRamdani <129932731+NabilaSyaidaRamdani@users.noreply.github.com>
2024-10-05 15:31:08 +0700

    Update main.py
    change the code

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 import streamlit as st
 
-st.title('Belajar Analisis Data dengan data Customer')
+st.title("Welcome to My Streamlit App!")
 
 st.header('HI! Our Dear Customer')
 
 st.subheader('Please fill Your Name')
 
-st.metric(label="Temperature", value="28 °C", delta="1.2 °C")
+Name = st.text_input(label='Full Name', value='')
 
+City = st.text_input(label='City', value='')
 
-name = st.text_input(label='Nama lengkap Customer', value='')
-st.write('Nama: ', name)
-
-
-number = st.number_input(label='Umur')
-st.write('Umur: ', int(number), ' tahun')
-
-
-if st.button('Say hello'):
-    st.write('Hello there')
+State = st.text_input(label='State', value='')
  
 with st.sidebar:
     
@@ -29,3 +21,27 @@ with st.sidebar:
         min_value=0, max_value=100, value=(0, 100)
     )
     st.write('Values:', values)
+    
+import streamlit as st
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url('https://i.pinimg.com/564x/ed/6b/be/ed6bbe5838979e5bca84c998cd2a87e6.jpg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+if st.button("Sign In"):
+    st.write("You clicked on Sign In!")
+    # Tambahkan logika untuk proses Sign In di sini
+
+# Tombol Sign Up
+if st.button("Sign Up"):
+    st.write("You clicked on Sign Up!")
+    # Tambahkan logika untuk proses Sign Up di sini
