77f12eef82c64a28154ca2008426d77cfd964b59
NabilaSyaidaRamdani <129932731+NabilaSyaidaRamdani@users.noreply.github.com>
2024-10-08 10:47:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -39,62 +39,16 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-if st.button("Sign In"):
-    st.write("You clicked on Sign In!")
-    # Tambahkan logika untuk proses Sign In di sini
-
+# Membuat tombol Sign In
+if st.button('Sign In'):
+    # Membuka tab baru menggunakan JavaScript
+    js = """
+    <script type="text/javascript">
+        window.open('https://your-sign-in-url.com', '_blank').focus();
+    </script>
+    """
+    st.markdown(js, unsafe_allow_html=True)
+ 
 # Tombol Sign Up
 if st.button("Sign Up"):
     st.write("You clicked on Sign Up!")
-
-
-
-
-import streamlit as st
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# Menghitung 10 kota teratas
-city_counts = data["customer_city"].value_counts().head(10)
-
-# Mengatur subheader
-st.subheader("Top 10 Cities by Number of Customers")
-
-# Membuat plot dengan subplots (seperti pada contoh kedua)
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 10))
-
-# Membuat barplot horizontal
-colors = ["#90CAF9" if i == 0 else "#D3D3D3" for i in range(len(city_counts))]
-sns.barplot(x=city_counts.values, y=city_counts.index, palette=colors, ax=ax)
-
-# Mengatur label dan title
-ax.set_xlabel("Number of Customers", fontsize=20)
-ax.set_title("Top 10 Cities by Number of Customers", loc="center", fontsize=30)
-ax.tick_params(axis='y', labelsize=15)
-ax.tick_params(axis='x', labelsize=15)
-
-# Menambahkan label ke setiap bar
-for index, value in enumerate(city_counts.values):
-    ax.text(value, index, str(value), fontsize=15)
-
-# Menampilkan plot di Streamlit
-st.pyplot(fig)
-
-
-
-
-
-tab1, tab2, tab3 = st.tabs(["Tab 1", "Tab 2", "Tab 3"])
- 
-with tab1:
-    st.header("Tab 1")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
- 
-with tab2:
-    st.header("Tab 2")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
- 
-with tab3:
-    st.header("Tab 3")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
-
