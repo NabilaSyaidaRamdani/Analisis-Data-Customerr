98cd97159862d2bbc27fb0249009e295f27f13ea
NabilaSyaidaRamdani <129932731+NabilaSyaidaRamdani@users.noreply.github.com>
2024-10-08 10:23:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -47,3 +47,18 @@ if st.button("Sign In"):
 if st.button("Sign Up"):
     st.write("You clicked on Sign Up!")
     # Tambahkan logika untuk proses Sign Up di sini
+st.title('Belajar Analisis Data')
+tab1, tab2, tab3 = st.tabs(["Tab 1", "Tab 2", "Tab 3"])
+ 
+with tab1:
+    st.header("Tab 1")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+ 
+with tab2:
+    st.header("Tab 2")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+ 
+with tab3:
+    st.header("Tab 3")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
+
